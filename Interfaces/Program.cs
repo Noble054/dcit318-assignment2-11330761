6b67fc3207dfc1b12af776f71f317ae17f9e8596
Albertnoble123 <kobi8583@gmail.com>
2025-08-06 18:19:45 +0000

    Add Interface to the final project

--- Interfaces/Program.cs
+++ Interfaces/Program.cs
@@ -1,1 +1,39 @@
-﻿
+﻿using System;
+
+interface IMovable
+{
+    void Move();
+}
+
+class Car:IMovable
+{
+    public void Move()
+    {
+        Console.WriteLine("The Car is moving");
+    }
+}
+
+class Bicycle : IMovable
+{
+    public void Move()
+    {
+        Console.WriteLine("Bicycle is moving");
+    }
+}
+
+class Program
+{
+    static void Main(String[] args)
+    {
+        IMovable car = new Car();
+        IMovable bicycle = new Bicycle();
+        bicycle.Move();
+        car.Move();
+
+        Console.WriteLine("Press any key to exit ....");
+        Console.ReadKey();
+
+    }
+    
+
+}
