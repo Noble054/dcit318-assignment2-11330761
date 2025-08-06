488a4fd457ab5f29ad6f0df76b0d42afd17a5cd7
Albertnoble123 <kobi8583@gmail.com>
2025-08-06 10:03:38 +0000

    Did the inheritance and method overriding

---
+++ Inheritance and Method Overriding/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+class Animal
+{
+    public virtual void MakeSound()
+    {
+        Console.WriteLine("Animal sounds");
+
+
+    }
+}
+
+class  Dog:Animal
+{
+    public override void MakeSound()
+    {
+        Console.WriteLine("Bark!! Bark!!! ");
+    }
+
+}
+
+class Cat:Animal
+{
+    public override void MakeSound()
+    {
+        Console.WriteLine("meow meow meow");
+    }
+}
+
+class  Program
+{
+   static void Main(string[] args)
+    {
+        Animal parentAnimal = new Animal();
+        Dog childdog = new Dog();
+        Cat childcat = new Cat();
+
+        parentAnimal.MakeSound();
+        childdog.MakeSound();
+        childcat.MakeSound();
+
+        Console.WriteLine("Press any key to exit...");
+        Console.ReadKey();
+    } 
+}   
